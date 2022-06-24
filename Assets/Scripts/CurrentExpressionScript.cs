f7281f1c8e287b2e8fa9afbaee17b7231894f121
Drung90 <drung90@gmail.com>
2022-06-24 19:05:05 +0300

    12
    Виправлено роботу backspace

---
+++ Assets/Scripts/CurrentExpressionScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CurrentExpressionScript : MonoBehaviour
+{
+    public bool nullText, nowB, operationBlocker, memoryReset, backSpaceBlock;
+    public double a, b, memoryCell, saveB;
+    public Text currentExpression, currentValue;
+    public string saveButtonText;
+    private void Start()
+    {
+        currentValue.text = "0";
+        nullText = true;
+    }
+    private void Update()
+    {
+        if (!nowB)
+        {
+            a = Convert.ToDouble(currentValue.text);
+        }
+        else
+        {
+            b = Convert.ToDouble(currentValue.text);
+        }
+    }
+}
